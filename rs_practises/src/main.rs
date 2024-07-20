4f863e3dcb44c4422c2219a6dd43f9c4a8e16e9f
berlinen <1292393108@qq.com>
2024-07-20 16:11:22 +0800

    feat: arc mutex

--- rs_practises/src/main.rs
+++ rs_practises/src/main.rs
@@ -1,8 +1,11 @@
 mod closure;
+mod mutex_arc;
 mod simple_code;
 
 fn main() {
     simple_code::simple_thread();
     closure::closure_test();
-    closure::closure_object_test()
+    closure::closure_object_test();
+    mutex_arc::arc_test();
+    mutex_arc::mutex_test();
 }
---
+++ rs_practises/src/mutex_arc.rs
@@ -0,0 +1,48 @@
+use std::sync::{
+    atomic::{AtomicI32, Ordering},
+    Arc, Mutex,
+};
+use std::thread;
+
+// #[derive(Debug)]
+// struct User<'a> {
+//     pub name: &'a str,
+// }
+
+pub fn arc_test() {
+    let a = Arc::new(AtomicI32::new(0));
+    let handle_a = a.clone();
+    let handle1 = thread::spawn(move || {
+        for _ in 0..1000 {
+            handle_a.fetch_add(1, Ordering::Relaxed);
+        }
+    });
+    let handle_b = a.clone();
+    let handle2 = thread::spawn(move || {
+        for _ in 0..1000 {
+            handle_b.fetch_add(1, Ordering::Relaxed);
+        }
+    });
+    handle1.join().unwrap();
+    handle2.join().unwrap();
+    println!("{:?} arc", a);
+}
+
+pub fn mutex_test() {
+    let a = Arc::new(Mutex::new(-1));
+
+    let mut handle = Vec::new();
+
+    for i in 0..5 {
+        let code = a.clone();
+        handle.push(thread::spawn(move || {
+            let mut data = code.lock().unwrap();
+            (*data) += i;
+            println!("{:?} mutex", data);
+        }))
+    }
+
+    for h in handle {
+        h.join().unwrap();
+    }
+}
