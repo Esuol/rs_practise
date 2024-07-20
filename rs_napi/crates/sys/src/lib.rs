41209c693a8fd407e0cbb62a3012a1fcbc674cdf
berlinen <1292393108@qq.com>
2024-07-20 17:59:22 +0800

    feat: rs ffi

--- rs_napi/crates/sys/src/lib.rs
+++ rs_napi/crates/sys/src/lib.rs
@@ -19,3 +19,14 @@ pub struct napi_env__ {
 pub type napi_env = *mut napi_env__;
 
 pub type napi_status = i32;
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct napi_callback_info__ {
+    _unused: [u8; 0],
+}
+
+pub type napi_callback_info = *mut napi_callback_info__;
+
+pub type napi_callback =
+    Option<unsafe extern "C" fn(env: napi_env, info: napi_callback_info) -> napi_value>;
