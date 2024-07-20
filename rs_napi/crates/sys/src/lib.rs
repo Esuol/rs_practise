fdc58a885565e9d05adeeab043752e0405f5cc0d
berlinen <1292393108@qq.com>
2024-07-20 17:51:47 +0800

    feat: rs ffi

--- rs_napi/crates/sys/src/lib.rs
+++ rs_napi/crates/sys/src/lib.rs
@@ -7,3 +7,15 @@ use std::os::raw::{c_char, c_int, c_uint, c_void};
 pub struct napi_value__ {
     _unused: [u8; 0],
 }
+
+pub type napi_value = *mut napi_value__;
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct napi_env__ {
+    _unused: [u8; 0],
+}
+
+pub type napi_env = *mut napi_env__;
+
+pub type napi_status = i32;
