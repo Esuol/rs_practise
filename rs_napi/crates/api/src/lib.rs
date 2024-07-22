2709c8ab278cf1852f3ca9c4409375ad0ebefad9
berlinen <1292393108@qq.com>
2024-07-22 20:46:25 +0800

    feat: rs ffi

--- rs_napi/crates/api/src/lib.rs
+++ rs_napi/crates/api/src/lib.rs
@@ -1,6 +1,6 @@
 use std::ffi::CString;
 use std::ptr;
-use sys::{napi_callback_info, napi_env, napi_value};
+use sys::{napi_callback_info, napi_env, napi_get_cb_info, napi_value};
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
