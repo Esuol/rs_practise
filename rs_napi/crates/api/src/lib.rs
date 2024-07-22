4a97f886d0dbb6522cc9f77578b9cd606a052d25
berlinen <1292393108@qq.com>
2024-07-22 21:13:55 +0800

    feat: add_unsafe_code

--- rs_napi/crates/api/src/lib.rs
+++ rs_napi/crates/api/src/lib.rs
@@ -1,7 +1,32 @@
 use std::ffi::CString;
 use std::ptr;
-use sys::{napi_callback_info, napi_env, napi_get_cb_info, napi_value};
+use sys::{napi_callback_info, napi_env, napi_value, napi_value__};
 
-pub fn add(left: usize, right: usize) -> usize {
+pub fn add(left: f64, right: f64) -> f64 {
     left + right
+}
+
+unsafe extern "C" fn add_unsafe_code(env: napi_env, callback: napi_callback_info) -> napi_value {
+    let mut a: f64 = 0.0;
+    let mut b: f64 = 0.0;
+
+    unsafe {
+        let mut args = [ptr::null_mut(); 2];
+        sys::napi_get_cb_info(
+            env,
+            callback,
+            &mut 2,
+            args.as_mut_ptr(),
+            ptr::null_mut(),
+            ptr::null_mut(),
+        );
+        sys::napi_get_value_double(env, args[0], &mut a);
+        sys::napi_get_value_double(env, args[1], &mut b);
+    };
+    let v = add(a, b);
+    let mut res = ptr::null_mut();
+    unsafe {
+        sys::napi_create_double(env, v, &mut res);
+    };
+    res
 }
