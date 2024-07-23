7e1ca846804492d274383844082124172ebdb250
berlinen <1292393108@qq.com>
2024-07-23 22:40:46 +0800

    feat: init napi

--- rs_napi/crates/apisecond/src/lib.rs
+++ rs_napi/crates/apisecond/src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: usize, right: usize) -> usize {
+mod register;
+mod value;
+
+use backend::api;
+
+#[api]
+pub fn add(left: f64, right: f64) -> f64 {
     left + right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[api]
+pub fn minus(left: f64, right: f64) -> f64 {
+    left - right
 }
