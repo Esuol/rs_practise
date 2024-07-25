46a74f27ef75a9b765a22d3a31e1d8d6a3dec51d
berlinen <1292393108@qq.com>
2024-07-25 22:19:56 +0800

    feat: update

--- rs_libuv/node_uv/build.rs
+++ rs_libuv/node_uv/build.rs
@@ -1,5 +1,8 @@
+// extern crate napi_build; 这一行引入了一个名为 napi_build 的外部 crate。
+// napi_build 是一个用于构建 Node.js 原生模块的工具库，它简化了与 Node.js 的集成过程。
 extern crate napi_build;
 
 fn main() {
+  // 这个函数调用配置并准备构建环境，以便正确地编译和链接 Node.js 原生模块。
   napi_build::setup();
 }
--- rs_libuv/node_uv/src/lib.rs
+++ rs_libuv/node_uv/src/lib.rs
@@ -31,15 +31,20 @@ impl Task for ComputeFib {
   }
 }
 
+// 在这段代码中，我们定义了一个名为 native_fib 的函数，它被标记为 #[js_function(0)]。这个标记表明该函数是一个 JavaScript 函数，可以从 JavaScript 环境中调用，并且它不接受任何参数（参数数量为 0）。
 #[js_function(0)]
 pub fn native_fib(ctx: CallContext) -> Result<JsObject> {
   let task = ComputeFib::new(20);
+  // 使用 ctx.env.spawn(task)? 将这个任务提交给 JavaScript 环境的异步运行时。spawn 方法会返回一个 async_promise，它是一个包含异步操作结果的承诺对象。
   let async_promise = ctx.env.spawn(task)?;
+  // 函数返回 async_promise.promise_object()，这是一个 JavaScript 的 Promise 对象，表示异步操作的结果。通过这种方式，Rust 代码可以与 JavaScript 代码进行异步交互，并返回一个 Promise，以便在 JavaScript 中处理异步计算的结果。
   Ok(async_promise.promise_object())
 }
 
+// 这个标记表明该函数用于导出模块中的方法，使其可以在 JavaScript 环境中使用。
 #[module_exports]
 pub fn register_js(mut exports: JsObject) -> Result<()> {
+  // create_named_method 方法将 native_fib 函数绑定到 exports 对象上，使其可以通过 exports.nativeUVFib 在 JavaScript 中调用。
   exports.create_named_method("nativeUVFib", native_fib)?;
   Ok(())
 }
