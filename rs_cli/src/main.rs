45dac071baa79add7f4feb2a5c7af94c25911f03
berlinen <1292393108@qq.com>
2024-07-26 22:35:04 +0800

    feat: update cli

--- rs_cli/src/main.rs
+++ rs_cli/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let source_file = std::env::args().nth(1).expect("Except source file path");
+    let dest_file = std::env::args().nth(2).expect("Except dest file path");
+
+    println!("Copying {} to {}", source_file, dest_file);
+    std::fs::copy(source_file, dest_file).unwrap();
 }
